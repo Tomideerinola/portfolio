45e20c3f1623e00e3f63d47c3b6e318c1d994c11
mac <mac@Mellys-Mac.local>
2026-01-14 21:50:39 -0800

    completed portfolio

--- port/models.py
+++ port/models.py
@@ -4,3 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 db=SQLAlchemy()
 
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20))
+    message = db.Column(db.Text, nullable=False)
+    contact_method = db.Column(db.String(20), nullable=False)
+    date_sent = db.Column(db.DateTime, default=datetime.utcnow)
+    contact_status = db.Column(db.String(50), default="unattended")  
--- port/route.py
+++ port/route.py
@@ -4,10 +4,34 @@ from flask_mail import Message
 from flask import render_template,redirect,request,url_for,make_response,session,flash,jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from port import app,csrf,mail
-from port.models import db
+from port.models import db,Contact
 from port.form import ContactForm
 
 
 @app.get('/')
 def home():
-    return 'Test'
+    contact= ContactForm()
+
+    return render_template('index.html',title="Home",contact=contact)
+
+@app.route('/contact/', methods=['GET', 'POST'])
+def contact():
+
+    contact= ContactForm()
+
+    if request.method == 'POST':
+        if contact.validate_on_submit():
+            name= contact.name.data
+            email = contact.email.data
+            message= contact.message.data
+            contact_method=contact.contact_method.data
+            phone = contact.phone.data
+
+            co=Contact(name=name,email=email,message=message,contact_method=contact_method,phone=phone)
+            db.session.add(co)
+            db.session.commit()
+            flash('Your Message has been received and I will get back to you Shortly', 'success ')
+            return redirect(url_for("contact"))
+        else:
+            flash('Please correct the errors in the form.', 'danger ')
+    return render_template('index.html',contact=contact)
